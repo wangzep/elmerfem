a114307a3b6ba3ba57c821c1c6b9a7c862d62d1b
geoffschrank <drgeoffschrank@gmail.com>
2017-03-28 16:43:36 -0400

    Fixed spatial distribution so that it is sinusoidal in both x and y directions.

--- fem/src/modules/Beta_Calc.F90
+++ fem/src/modules/Beta_Calc.F90
@@ -9,7 +9,7 @@
 !This function will get the Rubidium parameters from the SIF file and return
 !the Beta parameter. It encodes equation (A7)-(A10) from Fink et al.
 
-FUNCTION BetaCalc(Model, n, x, y) RESULT(initopt)
+FUNCTION BetaCalc(Model, n, x) RESULT(initopt)
     USE DefUtils
     IMPLICIT NONE
     TYPE(Model_t) :: Model
@@ -22,7 +22,7 @@ FUNCTION BetaCalc(Model, n, x, y) RESULT(initopt)
         absorb_laser_ratio,laser_frequency,&
         rubidium_frequency, laser_freq_width,&
         rubidium_freq_width, rubidium_wavelength,w_prime, w_dprime,&
-        w_input_real,w_input_imaginary, Beta, power, area, initopt, h, x, y,&
+        w_input_real,w_input_imaginary, Beta, power, area, initopt, h, x(3),&
 	pir
     LOGICAL :: FLAG, Found
     !------------------------------------------------------------------------!
@@ -90,7 +90,7 @@ FUNCTION BetaCalc(Model, n, x, y) RESULT(initopt)
     Beta = 2*DSQRT(PI*LOG2)*(electron_radius*oscillator_strength*&
         laser_wavelength**2*w_prime)/laser_linewidth
     !PRINT *,'Beta is', Beta
-    initopt = Beta*power/(area*h*laser_frequency)*COS((pir**(3/2)*SQRT(x**2 + y**2))/(2*SQRT(area)))
+    initopt = Beta*power/(area*h*laser_frequency)*COS((pir**(3/2)*SQRT(x(1)**2 + x(2)**2))/(2*SQRT(area)))
     !PRINT *,'Power is', power
     !PRINT *,'Area is', area
     !PRINT *,'h is', h
